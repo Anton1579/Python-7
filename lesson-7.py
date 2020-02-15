cce66a29342e851dedc57cf687ddb05f99791e94
Anton <anton.antonov1579@gmail.com>
2020-02-15 11:06:34 +0300

    lesson7.2

--- lesson-7.py
+++ lesson-7.py
@@ -45,3 +45,60 @@ result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
 print(my_matrix.__add__())
+
+
+# 2. Реализовать проект расчета суммарного расхода ткани на производство одежды.
+# Основная сущность (класс) этого проекта — одежда, которая может иметь определенное
+# название. К типам одежды в этом проекте относятся пальто и костюм. У этих типов
+# одежды существуют параметры: размер (для пальто) и рост (для костюма).
+# Это могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы:
+# для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3). Проверить работу этих методов
+# на реальных данных.Реализовать общий подсчет расхода ткани. Проверить на практике
+# полученные на этом уроке знания: реализовать абстрактные классы для основных классов
+# проекта, проверить на практике работу декоратора @property.
+
+class Clothes:
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+    def result_a(self):
+        return self.width / 6.5 + 0.5
+
+    def result_b(self):
+        return 2 * self.length + 0.3
+
+    @property
+    def get_cl(self):
+        return str(f'Общее количество ткани - {(self.width / 6.5 + 0.5) + (2 * self.length + 0.3)}')
+
+class Coat(Clothes):
+    def __init__(self, width, length):
+        super().__init__(width, length)
+        self.result_1 = round((self.width / 6.5 + 0.5))
+
+    def __str__(self):
+        return f'Количество ткани на пальто - {self.result_1}'
+
+class Costume(Clothes):
+    def __init__(self, width, length):
+        super().__init__(width, length)
+        self.result_2 = round(2 * self.length + 0.3)
+
+
+    def __str__(self):
+        return f'Количество ткани на костюм - {self.result_2}'
+
+
+
+coat = Coat(30, 9)
+costume = Costume(10, 5)
+print(coat)
+print(costume)
+print(coat.get_cl)
+print(costume.get_cl)
+print(f'Костюм - {costume.result_a()}')
+print(f'Костюм - {costume.result_b()}')
+print(f'Пальто - {coat.result_a()}')
+print(f'Пальто - {coat.result_b()}')
